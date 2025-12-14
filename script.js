47257302f930059d47f851d81cb0b1d625b95ee7
nkirotenehema352-gif <nkirotenehema352@gmail.com>
2025-12-14 16:31:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function changetext(){
+    document.getElementById("about-text")inner text=
+    "thanks for clicking!this text changed using javascript."
+}
+funtion showemail(){
+    document.getElementById("email").inner=
+    "email;muturineema460@gmail.com";
+}
